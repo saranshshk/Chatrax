cf5082bc5e7c498c5a831f2c358894bb70db2513
saranshshk <saranshsh7658@gmail.com>
2026-04-05 22:21:21 +0530

    Make Login responsive

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
     e.preventDefault();
 
     if (!form.username || !form.password) {
-      toast.error("Please fill all field", {toastId: "one-toast-only"});
+      toast.error("Please fill all field", { toastId: "one-toast-only" });
       return;
     }
 
@@ -35,62 +35,63 @@ export default function Login() {
       }
     } catch (err) {
       console.error(err);
-      toast.error("Invalid username or password",{toastId: "one-toast-only"});
+      toast.error("Invalid username or password", {
+        toastId: "one-toast-only",
+      });
     } finally {
       setloading(false);
     }
   };
   return (
     <>
-      <ToastContainer autoClose={1500 }  />
-      <div className={styles.container}>
-        <div className={styles.left}>
-          <div className={styles.bg}></div>
-          <div className={styles.tagline}>
+      <ToastContainer autoClose={1500} />
+      <div className={styles.body}>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <div className={styles.bg}></div>
+            <div className={styles.tagline}>
             <h2>Connect. Chat. Share.</h2>
-            <p>Experience seamless conversations that feel alive 💬</p>
-          </div>
-        </div>
-        <div className={styles.right}>
-          <h1 className={styles.heading}>Chatrax</h1>
-          <div className={styles.right_container}>
-            <div className={styles.input_con}>
-              <form onSubmit={handlelogin}>
-                <input
-                  type="text"
-                  placeholder="Email or Username"
-                  name="username"
-                  value={form.username}
-                  onChange={handlechange}
-                  className={styles.email}
-                />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  value={form.password}
-                  onChange={handlechange}
-                  className={styles.password}
-                />
-
-                <div className={styles.forget}>
-                  <Link to="/Forget-Password">Forget Password</Link>
-                </div>
-
-                <button
-                  type="submit"
-                  disabled={loading}
-                  className={styles.login}
-                >
-                  {loading ? "Logging in..." : "Login"}
-                </button>
-              </form>
-             </div>
-             <Link to="/Signup" className={styles.Signup}> 
-               Don't have a Account? <span> Sign Up</span> 
-            </Link> 
-
             
+            <p>Experience seamless conversations that feel alive 💬</p>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.heading}>Chatrax</h2>
+            {/* <div className={styles.right_container}> */}
+            {/* <div className={styles.input_con}> */}
+            <form onSubmit={handlelogin}>
+              <input
+                type="text"
+                placeholder="Email or Username"
+                name="username"
+                value={form.username}
+                onChange={handlechange}
+                className={styles.inputbox}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={form.password}
+                onChange={handlechange}
+                className={styles.inputbox}
+              />
+
+              <div className={styles.forget} >
+                <Link to="/Forget-Password" className={styles.forget}>Forget Password</Link>
+           </div>
+
+              <button type="submit" disabled={loading} className={styles.btn}>
+                {loading ? "Logging in..." : "Login"}
+              </button>
+            </form>
+            
+            <div className={styles.bottomtext}>
+                Don't have a Account? 
+              <Link to="/Signup" >
+               Sign Up
+              </Link>
+            </div>
           </div>
         </div>
       </div>
