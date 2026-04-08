1df8eb69323cff67b0e835a9c188f5a7d624bfda
saranshshk <saranshsh7658@gmail.com>
2026-04-08 20:10:48 +0530

    Add route of Otp Verification

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { Routes,Route } from "react-router-dom"
 import PrivateRoute from "./components/Route/PrivateRoute"
 import PublicRoute from "./components/Route/PublicRoute"
 import Signup from "./components/signup/Signup"
+import VerifyOtp from "./components/Otp/Otp"
 function App(){
   return(
   <div>
     <Routes>
       <Route path="/"element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
-      
-
+      <Route path="/VerifyOtp"element={<VerifyOtp/>}/>
     </Routes>
   </div>
   )
