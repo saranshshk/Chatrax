b76aada571465d4b6cd5790b6e2ac7a843aed81f
saranshshk <saranshsh7658@gmail.com>
2026-04-11 13:46:11 +0530

    Change Navigate of auto redirect

--- src/components/Route/PublicRoute.jsx
+++ src/components/Route/PublicRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom"
 export default function PublicRoute({children}){
     const token = localStorage.getItem("authToken")
     if (token){
-        return <Navigate to="/login"replace/>
+        return <Navigate to="/"replace/>
     }
     return children;
     
