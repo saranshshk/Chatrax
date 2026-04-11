41149392c99050b21d2a2b78559556bbf4935baa
saranshshk <saranshsh7658@gmail.com>
2026-04-11 13:49:14 +0530

    Add Some Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import PublicRoute from "./components/Route/PublicRoute"
 import Signup from "./components/signup/Signup"
 import VerifyOtp from "./components/Otp/Otp"
 import Forget from "./components/ForgetPassword/Forget"
+import ChatMain from "./components/Chat/ChatMain"
 function App(){
   return(
   <div>
     <Routes>
-      <Route path="/"element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/VerifyOtp"element={<VerifyOtp/>}/>
+      <Route path="/" element={<PrivateRoute><ChatMain/></PrivateRoute>}/>
+      <Route path="/Login"element={<PublicRoute><Login/></PublicRoute>}/>
+      <Route path="/signup" element={<PublicRoute><Signup/></PublicRoute>}/>
+      <Route path="/VerifyOtp"element={<PublicRoute><VerifyOtp/></PublicRoute>}/>
       <Route path="/Forget-Password"element={<Forget/>}/>
     </Routes>
   </div>
